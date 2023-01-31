9b98c391ac5d4437010b352e1496a8227d6d16d9
Diego Gomez <dngomez.e@gmail.com>
2023-01-31 16:58:10 -0300

    Test without baseurl in vite config

--- src/main.jsx
+++ src/main.jsx
@@ -19,7 +19,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider>
       <AuthProvider>
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <BrowserRouter>
           <Routes>
             <Route path="/" element={<Layout />}>
               <Route index element={<Home />} />
