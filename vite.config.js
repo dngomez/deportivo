c1c9b874305256acf355e4fafa7a60c97d26368c
Diego Gomez <dngomez.e@gmail.com>
2023-08-22 20:55:14 -0400

    Changed backend

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    host: "0.0.0.0"
+    host: "0.0.0.0",
+    proxy: {
+      '^/api/.*': {
+        target: "http://back:5000",
+        changeOrigin: true
+      }
+    }
   },
   css: {
     preprocessorOptions: {
